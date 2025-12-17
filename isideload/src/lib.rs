01d3872181289d82cba878f91bf54e1540115c46
nab138 <nab@nabdev.me>
2025-12-16 20:35:44 -0500

    add idevice and plist deps

--- isideload/src/lib.rs
+++ isideload/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use idevice::plist;
+use plist::Value;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn test() -> Value {
+    plist!("value")
 }
