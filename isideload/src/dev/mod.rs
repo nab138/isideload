338388579cf1af92c83fe52b7cba5125509fe9e2
nab138 <nab@nabdev.me>
2026-09-23 16:27:46 -0400

    Normalize app id names

--- isideload/src/dev/mod.rs
+++ isideload/src/dev/mod.rs
@@ -5,3 +5,11 @@ pub mod developer_session;
 pub mod device_type;
 pub mod devices;
 pub mod teams;
+
+// some non-alphanumeric characters cause Developer error 35: An invalid value was provided for the parameter 'appIdName'.
+pub fn normalize_app_names(name: &str) -> String {
+    name.chars()
+        .filter(|c| c.is_ascii_alphanumeric() || *c == ' ')
+        .map(|c| if c == ' ' { '_' } else { c })
+        .collect()
+}
